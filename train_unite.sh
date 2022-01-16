2dc467997233f6b56dd40a215e95b0c06ada1a68
Colin Brislawn <cbrisl@gmail.com>
2022-01-16 13:15:43 -0500

    Rename step1 folder

--- train_unite.sh
+++ train_unite.sh
@@ -6,22 +6,22 @@
 ## Version, date, taxon, refS, repS, status, DOI
 ## Other wildcards: URI folder path / prefix, filename
 # 8.3	2021-05-10	Fungi	14 097	44 343	Current	https://doi.org/10.15156/BIO/1264708
-wget -P step1_download_PlutoF https://files.plutof.ut.ee/public/orig/C5/54/C5547B97AAA979E45F79DC4C8C4B12113389343D7588716B5AD330F8BDB300C9.tgz
+wget -P step1_download https://files.plutof.ut.ee/public/orig/C5/54/C5547B97AAA979E45F79DC4C8C4B12113389343D7588716B5AD330F8BDB300C9.tgz
 # 8.3	2021-05-10	Fungi	14 097	83 993	Current	https://doi.org/10.15156/BIO/1264763
-#wget -P step1_download_PlutoF https://files.plutof.ut.ee/public/orig/B3/05/B3054DF783AC61A0C3BD0FDEB0516EC394934809AAE43CA0F3081C0A184FAA39.tgz
+#wget -P step1_download https://files.plutof.ut.ee/public/orig/B3/05/B3054DF783AC61A0C3BD0FDEB0516EC394934809AAE43CA0F3081C0A184FAA39.tgz
 # 8.3	2021-05-10	All eukaryotes	14 237	96 423	Current	https://doi.org/10.15156/BIO/1264819
-#wget -P step1_download_PlutoF https://files.plutof.ut.ee/public/orig/48/29/4829D91F763E20F0F4376A60AA53FC9FBE6029A7D1BDC1B45347DD64EDE5D560.tgz
+#wget -P step1_download https://files.plutof.ut.ee/public/orig/48/29/4829D91F763E20F0F4376A60AA53FC9FBE6029A7D1BDC1B45347DD64EDE5D560.tgz
 # 8.3	2021-05-10	All eukaryotes	14 237	190 888	Current	https://doi.org/10.15156/BIO/1264861
-#wget -P step1_download_PlutoF https://files.plutof.ut.ee/public/orig/1D/31/1D31FA3A308BDC2FB2750D62C0AA40C5058C15405A3CC5C626CC3A3F5E3903ED.tgz
+#wget -P step1_download https://files.plutof.ut.ee/public/orig/1D/31/1D31FA3A308BDC2FB2750D62C0AA40C5058C15405A3CC5C626CC3A3F5E3903ED.tgz
 
 
 #Decompress. The downloaded file is actually a tar.gz file and so needs to be decompressed with:
 # These each go into seperate folders, so we should track that
 ## Wildcards: filename, output folder
-tar xzf step1_download_PlutoF/C5547B97AAA979E45F79DC4C8C4B12113389343D7588716B5AD330F8BDB300C9.tgz -C step2_extract
-#tar xzf step1_download_PlutoF/B3054DF783AC61A0C3BD0FDEB0516EC394934809AAE43CA0F3081C0A184FAA39.tgz -C step2_extract
-#tar xzf step1_download_PlutoF/4829D91F763E20F0F4376A60AA53FC9FBE6029A7D1BDC1B45347DD64EDE5D560.tgz -C step2_extract
-#tar xzf step1_download_PlutoF/1D31FA3A308BDC2FB2750D62C0AA40C5058C15405A3CC5C626CC3A3F5E3903ED.tgz -C step2_extract
+tar xzf step1_download/C5547B97AAA979E45F79DC4C8C4B12113389343D7588716B5AD330F8BDB300C9.tgz -C step2_extract
+#tar xzf step1_download/B3054DF783AC61A0C3BD0FDEB0516EC394934809AAE43CA0F3081C0A184FAA39.tgz -C step2_extract
+#tar xzf step1_download/4829D91F763E20F0F4376A60AA53FC9FBE6029A7D1BDC1B45347DD64EDE5D560.tgz -C step2_extract
+#tar xzf step1_download/1D31FA3A308BDC2FB2750D62C0AA40C5058C15405A3CC5C626CC3A3F5E3903ED.tgz -C step2_extract
 
 
 #Move into the developer directory for one project
